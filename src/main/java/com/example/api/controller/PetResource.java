1196d0713c4f0e46298d775e990e5a576964bfdf
atomobianco <tommaso.bianco@gmail.com>
2014-05-06 22:11:14 +0200

    Added annotations to pet resource.

--- src/main/java/com/example/api/controller/PetResource.java
+++ src/main/java/com/example/api/controller/PetResource.java
@@ -1,10 +1,17 @@
 package com.example.api.controller;
 
 import com.example.api.entity.Pet;
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
 
 import javax.ws.rs.core.UriInfo;
+import java.util.LinkedHashMap;
 
+@ApiModel(value = "Pet", description = "Pet resource", parent = LinkedHashMap.class)
 public class PetResource extends Link {
+
+    @ApiModelProperty(value = "Pet name") public String getName() { return (String) get("name"); }
+    @ApiModelProperty(value = "Pet URL")  public String getHref() { return (String) get("href"); }
 
     public PetResource(UriInfo info, Pet pet) {
         super(info, pet);
