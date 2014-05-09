3985cf48475963f6705e71851a327b0218300f99
atomobianco <tommaso.bianco@gmail.com>
2014-05-09 19:39:07 +0200

    Added GET all to controller.

--- src/main/java/com/example/api/controller/PetController.java
+++ src/main/java/com/example/api/controller/PetController.java
@@ -10,13 +10,32 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 @Path(Link.PETS)
 @Api(value = "/pets", description = "Operations about pets")
 @Produces({"application/json"})
+@Consumes({"application/json"})
 public class PetController extends BaseController {
 
     private PetService petService = DefaultPetService.getInstance();
+
+    @ApiOperation(value = "Find all pets")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public CollectionResource getPets(@Context UriInfo info) {
+        Collection<Pet> pets = petService.getPets();
+        if (pets == null || pets.size() == 0) {
+            return new CollectionResource(info, Link.PETS, Collections.emptyList());
+        }
+        Collection items = new ArrayList(pets.size());
+        for( Pet pet : pets ) {
+            items.add(new Link(info, pet));
+        }
+        return new CollectionResource(info, Link.PETS, items);
+    }
 
     @ApiOperation(value = "Find pet by ID", response = PetResource.class)
     @ApiResponses(value = {@ApiResponse(code = 400, message = "Invalid ID supplied"), @ApiResponse(code = 404, message = "Pet not found") })
@@ -36,7 +55,7 @@ public class PetController extends BaseController {
     @ApiImplicitParams({@ApiImplicitParam(name = "body", value = "Pet object that needs to be updated in the store", required = true, dataType = "PetResource", paramType = "body")})
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(@Context UriInfo info, @ApiParam(access = "internal") Pet pet) {
+    public Response createPet(@Context UriInfo info, @ApiParam(access = "internal") Pet pet) {
         pet = petService.save(pet);
         PetResource resource = new PetResource(info, pet);
         return created(resource);
